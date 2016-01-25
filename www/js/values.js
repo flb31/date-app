366a496da30e41e33a7065791335b8eb5c06c327
Fabian Luna <flb031@gmail.com>
2016-01-25 01:03:09 -0500

    Actualizado base de datos local de prueba

--- www/js/values.js
+++ www/js/values.js
@@ -1,58 +1,58 @@
 var data = new Array();
-data["1128053244"] =
+data["123456"] =
   {
-    "id" : "1128053244",
-    "name" : "Fabian Luna",
-    "email" : "flb031@gmail.com",
-    "phone" : "(+57)30100000",
+    "id" : "123456",
+    "name" : "Pedro Perez",
+    "email" : "pedro@gmail.com",
+    "phone" : "(+57)1234567",
     "dates" : [
       {
         "id" : "50",
         "date" : "2016-01-20T17:35:00-0500",
-        "title" : "Examen de los ojos",
-        "doctor" : "Fulanito de tal",
-        "office" : "20"
+        "title" : "Examen de la vista",
+        "doctor" : "Alberto Sierra",
+        "office" : "200"
       },
       {
         "id" : "100",
         "date" : "2016-02-25T15:30:00-0500",
         "title" : "Examen Fisico",
-        "doctor" : "Fulanito de tal",
+        "doctor" : "Juan Carlos Barrios",
         "office" : "204"
       }
     ]
   };
 
-data["123456"] =
+data["9876"] =
   {
-    "id" : "123456",
-    "name" : "Pepito Perez",
-    "email" : "pepito@gmail.com",
+    "id" : "9876",
+    "name" : "Andres Salamanca",
+    "email" : "andres@gmail.com",
     "phone" : "44444444",
     "dates" : [
       {
         "id" : "35",
         "date" : "2016-02-20T12:00:00-0500",
-        "title" : "Examen de los ojos",
-        "doctor" : "Juanita de tal",
-        "office" : "33"
+        "title" : "Analisis de sangre",
+        "doctor" : "Tatiana Bustamante",
+        "office" : "333"
       },
       {
         "id" : "36",
         "date" : "2016-03-01T12:00:00-0500",
-        "title" : "Examen de los ojos 2",
-        "doctor" : "Juanita de tal",
-        "office" : "33"
+        "title" : "Analisis de Sangre (2)",
+        "doctor" : "Tatiana Bustamante",
+        "office" : "333"
       }
     ]
   };
 
-data["1212"] =
+data["1122"] =
   {
-    "id" : "1212",
-    "name" : "Andrea J",
+    "id" : "1122",
+    "name" : "Andrea Jimenez",
     "email" : "andrea@gmail.com",
-    "phone" : "555",
+    "phone" : "5555555",
     "dates" : [
 
     ]
